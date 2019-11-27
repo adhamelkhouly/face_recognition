8b28439b927184850b861e40da09fa553dab5297
Adham El-Khouly <adham.elkhouly@hotmail.com>
2019-11-27 18:44:50 -0500

    preliminary version of the helpers class

--- helpers.py
+++ helpers.py
@@ -1,27 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from sklearn import svm
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
 import glob
-
-# Defining Directory Paths
-training_images_directories = ['./images/Abdullah/training/', 
-                               './images/Mustafa/training/',
-                               './images/Saleh/training/',
-                               './images/Adham/training/']
-testing_images_directories = ['./images/Abdullah/testing/',
-                              './images/Mustafa/testing/',
-                              './images/Saleh/testing/',
-                              './images/Adham/testing/']
-
-# Load the cascade for facial detection
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-
 
 class ImageManager:
     def __init__(self):
@@ -36,20 +16,134 @@ class ImageManager:
                                            './images/Adham/testing/']
         # Load the cascade for facial detection
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
+        
+        self.training_bounds = []
+        self.testing_bounds = []
+        self.training_faces = []
+        self.testing_faces = []
+        self.training_images = None
+        self.testing_images = None
+    
+    def extract_face(self, img, bounds):
+        """
+        Function takes in an image and bounds dict and returns a face
+        """
+        min_x =  bounds["min_x"]
+        max_x =  bounds["max_x"]
+        min_y =  bounds["min_y"]
+        max_y =  bounds["max_y"]
+        return img[min_y:max_y, min_x:max_x]
         
     def get_training_images(self, color=cv2.COLOR_BGR2RGB):
+        training_images = []
         # Populate Training Image list
         for directory in self.training_images_directories:
             images = [cv2.imread(file) for file in glob.glob("{directory}*.jpg".format(directory=directory))]
             for img in images:
                 training_images.append(cv2.cvtColor(img, color))
+        self.training_images = training_images
         return training_images
     
-    def get_training_images(self, color=cv2.COLOR_BGR2RGB):
-        # Populate Training Image list
+    def get_testing_images(self, color=cv2.COLOR_BGR2RGB):
+        testing_images = []
+        # Populate Testing Image list
         for directory in self.testing_images_directories:
             images = [cv2.imread(file) for file in glob.glob("{directory}*.jpg".format(directory=directory))]
             for img in images:
-                training_images.append(cv2.cvtColor(img, color))
-        return training_images
+                testing_images.append(cv2.cvtColor(img, color))
+        self.testing_images = testing_images
+        return testing_images
+    
+    def update_training_bounds(self):
+        training_bounds = []
+        result_images = []
+        for i, img in enumerate(self.training_images):     
+            img_disp = img.copy()
+            
+            if len(img.shape) == 3:
+                img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+                # Detect harris corners
+                corners = cv2.cornerHarris(img_gray, 2, 3, 0.04)
+            else:
+                # Detect harris corners
+                corners = cv2.cornerHarris(img, 2, 3, 0.04)
+
+            # Normalize corner map [0,1]
+            cv2.normalize(corners, corners, 0, 1, cv2.NORM_MINMAX)
+
+            # Define threshold for an optimal value
+            thres = 0.5
+
+            # List all points higher than threshold
+            loc = np.where(corners >= thres)
+            
+            pts = []
+            # Loop though points
+            for pt in zip(*loc[::-1]):
+                pts.append(pt)
+                # draw filled circle on each point
+                cv2.circle(img_disp, pt, 4, (255,0,0), -1)
+           
+            xs, ys = np.array(list(zip(*pts)))
+            bounds_dict = {"min_x" : np.amin(xs),
+                           "min_y" : np.amin(ys),
+                           "max_x" : np.amax(xs),
+                           "max_y" : np.amax(ys)}
+            
+            result_images.append(img)
+            training_bounds.append(bounds_dict)
+            face = self.extract_face(img, bounds_dict)
+            face = cv2.resize(face, (128,128))
+            self.training_faces.append(face)  
+            
+        self.training_bounds = training_bounds
+        
+        return result_images, training_bounds
+    
+    def update_testing_bounds(self):
+        testing_bounds = []
+        result_images = []
+        for i, img in enumerate(self.testing_images):
+            img_disp = img.copy()
+            
+            if len(img.shape) == 3:
+                img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+                # Detect harris corners
+                corners = cv2.cornerHarris(img_gray, 2, 3, 0.04)
+            else:
+                # Detect harris corners
+                corners = cv2.cornerHarris(img, 2, 3, 0.04)
+                
+            # Normalize corner map [0,1]
+            cv2.normalize(corners, corners, 0, 1, cv2.NORM_MINMAX)
+
+            # Define threshold for an optimal value
+            thres = 0.5
+
+            # List all points higher than threshold
+            loc = np.where(corners >= thres)
+            
+            pts = []
+            # Loop though points
+            for pt in zip(*loc[::-1]):
+                pts.append(pt)
+                # draw filled circle on each point
+                cv2.circle(img_disp, pt, 4, (255,0,0), -1)
+           
+            xs, ys = np.array(list(zip(*pts)))
+            bounds_dict = {"min_x" : np.amin(xs),
+                           "min_y" : np.amin(ys),
+                           "max_x" : np.amax(xs),
+                           "max_y" : np.amax(ys)}
+            
+            result_images.append(img_disp)
+            testing_bounds.append(bounds_dict)
+            face = self.extract_face(img, bounds_dict)
+            face = cv2.resize(face, (128,128))
+            self.testing_faces.append(face)  
+            
+        self.testing_bounds = testing_bounds
+        
+        return result_images, testing_bounds
